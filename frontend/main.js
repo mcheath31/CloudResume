4416a769785b966e0f61e85d55c1c4d36bd3d3b6
mcheath31 <heath.marc17@gmail.com>
2022-07-17 19:08:00 -0400

    First commit

---
+++ frontend/main.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', (event) =>{
+    getVisitCount();
+})
+const functionApiUrl = ''
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApiUrl).then(response => {
+        return response.json()
+    }).then(response =>{
+        console.log("website called function API.");
+        count = response.resumeCounter;
+        document.getElementById("1").innerText = count;
+    }).catch(function(error){
+        console.log(error);
+    })
+    return count;
+}
